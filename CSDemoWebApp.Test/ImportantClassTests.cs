405a0e7414c177ebcf8839e575eacc0f0d5d1410
Aviv Giladi <avivg@cloudshare.com>
2014-06-09 15:24:22 +0300

    added testing project

---
+++ CSDemoWebApp.Test/ImportantClassTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CSDemoWebApp.Models;
+using NUnit.Framework;
+
+namespace CSDemoWebApp.Test
+{
+
+    [TestFixture]
+    public class ImportantClassTests
+    {
+        [TestCase]
+        public void AdditionTest()
+        {
+            var obj = new ImportantClass();
+            Assert.AreEqual(obj.GetNum(), 0);
+            obj.AddToNum(13);
+            Assert.AreEqual(obj.GetNum(), 13);
+        }
+        [TestCase]
+        public void SubtractionTest()
+        {
+            var obj = new ImportantClass();
+            Assert.AreEqual(obj.GetNum(), 0);
+            obj.SubFromNum(-13);
+            Assert.AreEqual(obj.GetNum(), 13);
+        }
+        [TestCase]
+        public void AddSubTest()
+        {
+            var obj = new ImportantClass();
+            Assert.AreEqual(obj.GetNum(), 0);
+            obj.SubFromNum(13);
+            obj.AddToNum(13);
+            Assert.AreEqual(obj.GetNum(), 0);
+        }
+        [TestCase]
+        public void ResetTest()
+        {
+            var obj = new ImportantClass();
+            Assert.AreEqual(obj.GetNum(), 0);
+            obj.AddToNum(13);
+            obj.ResetNum();
+            Assert.AreEqual(obj.GetNum(), 0);
+        }
+    }
+}
---
+++ CSDemoWebApp/ImportantClass.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CSDemoWebApp
+{
+    public class ImportantClass
+    {
+        private int num;
+
+        public ImportantClass()
+        {
+            this.num = 0;
+        }
+
+        public int GetNum()
+        {
+            return this.num;
+        }
+
+        public void AddToNum(int add)
+        {
+            this.num += add;
+        }
+
+        public void SubFromNum(int sub)
+        {
+            this.num -= sub;
+        }
+
+        public void ResetNum()
+        {
+            this.num = 0;
+        }
+    }
+}
